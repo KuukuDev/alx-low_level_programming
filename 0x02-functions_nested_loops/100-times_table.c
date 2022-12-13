09866c7b0323a91cb4eb3fa6e9e5e8d57ae91eab
KuukuDev <owurakubuabeng777@gmail.com>
2022-12-13 08:22:22 -0800

    correction

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -34,7 +34,7 @@ void print_times_table(int n)
 					{
 						_putchar(' ');
 						_putchar('0' + (product / 10));
-						_putchat('0' + (product % 10));
+						_putchar('0' + (product % 10));
 					}
 					else if (product >= 100)
 					{
