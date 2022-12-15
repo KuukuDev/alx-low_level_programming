463b9d43e6a179f098ae2f74aae5a459db47814c
KuukuDev <owurakubuabeng777@gmail.com>
2022-12-15 07:58:12 -0800

    task 8 and 9 changes

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -2,17 +2,25 @@
 /**
  * print_square - function to print a square
  * @size: takes in the size of the square
+ * Return: Always 0.
  */
 void print_square(int size)
 {
-	int i j;
+	int x, y;
 
-	if (size <= 0)
-		_putchar('\n');
-	for (i = 0; i < size; i++)
+	if (size > 0)
 	{
-		for (j = 0; j < size; j++)
-			_putchar('#');
+		for (x = 1; x <= size; x++)
+		{
+			for (y = 1; y <= size; y++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
 		_putchar('\n');
 	}
 }
--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,31 +1,34 @@
 #include <stdio.h>
 /**
  * main - entry point
- * @void: no argument
  * Return: 0 Success
  */
 int main(void)
 {
 	int i;
 
-	for (i = 1; i < 100; i++)
-		if (i % 15 == 0)
+	for (x = 1; x <= 100; x++)
+	{
+		if (x % 3 == 0 || x % 5 == 0)
 		{
-			printf("FizzBuzz ");
-		}
-		else if (i % 5 == 0)
-		{
-			printf("Buzz ");
-		}
-		else if (i % 3 == 0)
-		{
-			printf("fizz ");
+			if (x % 3 == 0)
+			{
+				printf("Fizz");
+			}
+			if (x % 5 == 0)
+			{
+				printf("Buzz");
+			}
 		}
 		else
 		{
-			printf("%d ", i);
+			printf("%d", x);
 		}
-	printf("Buzz");
-	printf("\n");
+		if (x != 100)
+		{
+			putchar(' ');
+		}
+	}
+	putchar('\n');
 	return (0);
 }
