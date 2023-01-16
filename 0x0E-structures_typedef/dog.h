207d77a34bb8ec3a1cea406e26ecf5330814dbfa
KuukuDev <owurakubuabeng777@gmail.com>
2023-01-16 08:48:38 +0000

    Mandatory Tasks

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,20 +1,29 @@
-#ifndef DOG_H
-#define DOG_H
+#ifndef _HEADER_
+#define _HEADER_
+
 /**
- *struct dog - a new type
- *@name : string
- *@age : float
- *@owner : string
- *Description: a new type that contains dog name, age, and owner.
+ * struct dog - Dog attributes
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
+ *
+ * Description: The attributes of a dog.
  */
-typedef struct dog
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-} dog_t;
+};
+
+/**
+ * dog_t - Typedef for dog structure
+ */
+typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+
 #endif
